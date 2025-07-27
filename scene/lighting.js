f5ed5072a0f44a0e9289d120dfd7f99110f7ee05
fabio-gabriel <fabiogabrieleig@gmail.com>
2025-07-26 23:03:11 -0300

    adds missing TODO

--- scene/lighting.js
+++ scene/lighting.js
@@ -1,2 +1,3 @@
 // This is a lighting toggle. By default, lighting is handled by IBL (Image-based Lighting). This allows nice reflections in the water, shadows and directional lighting based on the skyboxes brightest point (the sun)
 
+// TODO
